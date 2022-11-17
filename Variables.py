27e887d52721615a587a351c23ff0c6ba8ce23ad
Mehmet OLGUN <71698654+seylab@users.noreply.github.com>
2022-11-17 20:32:54 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,57 @@
+sayi=26
+
+deger=222 # deger değişkeni sayı (int) türünde
+print(deger)
+deger = "Anadolu" # deger değişkeni şimdi metin
+print(deger)
+
+
+"""
+    Değişken isimleri a, b, c gibi kısa isimler olabileceği gibi, bu_uzun_bir_degisken_ismidir gibi 
+uzun olarak da tanımlanabilir.
+    Değişken isimleri sayı ile başlayamaz.
+    Harf ya da “_” alt çizgi ile başlayabilir.
+    Değişkenler büyük/küçük harf duyarlıdır. Yani Anadolu ve anadolu iki farklı değişken olarak 
+tanımlanır.
+    Değişken isimlerinde ( -, +, /, *, #, ?, .) ve boşluk gibi özel karakterler kullanılamaz.
+"""
+
+sayi = 26222
+noktali_sayi = 26.22
+metin = "Programlama öğreniyorum"
+_sayi= 200
+
+print(sayi)
+print(noktali_sayi)
+print(metin)
+print(_sayi)
+
+
+
+
+""" Çoklu değer atama """
+
+var1= var2= var3=20
+print(var1)
+print(var2)
+print(var3)
+      
+num1, num2, num3, string1= 15, 11,21, "Eskişehir"
+
+print(num1)
+print(num2)
+print(num3)
+print(string1)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tabs_Comment_Lines.py
@@ -0,0 +1,22 @@
+
+
+if True:
+ print('Hello')
+ a = 5
+ 
+if True: print('Hello'); a = 5
+
+# Bu bir yorum satırıdır
+# Merhaba yazdır
+print('Merhaba')
+
+
+# Bu uzun bir yorum örneğidir
+# ve bunun için birden çok
+# satır kullanılmıştır
+
+""" Bu yorum satırları da 
+çok satırlı yorumlar için
+güzel bir örnek olabilir """
+
+
