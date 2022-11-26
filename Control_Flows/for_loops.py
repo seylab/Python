c6ae1af5933081086bda40d19bf0b87cc9702799
Mehmet OLGUN <71698654+seylab@users.noreply.github.com>
2022-11-26 17:59:53 +0300

    added packages

---
+++ Control_Flows/for_loops.py
@@ -0,0 +1,13 @@
+# Listedeki tüm sayıların toplamını bulan Python kodu
+
+# sayı listesi
+numbers = [11, 26, 20, 34, 42, 77, 55, 3, 4, 91]
+
+# toplam değeri tutan değişken
+total = 0
+
+#  liste üzerinde işlem
+for num in numbers:
+    total = total + num
+
+print("Total", total)
--- Docstrings.py
+++
@@ -1,4 +0,0 @@
-def double(num):
-    """double değer hesaplayan fonksiyon"""
-    return 2*num
-print(double.__doc__)
