ff1b8155417f5e5087059c5e58a2a4d69507950d
Mehmet OLGUN <71698654+seylab@users.noreply.github.com>
2022-11-26 17:04:36 +0300

    added control flows

---
+++ Control_Flows.py
@@ -0,0 +1,12 @@
+# Sayı pozitif ise uygun bir mesaj yazdırıyoruz
+
+sayı = 3
+if sayı > 0 :
+    print(sayı, "pozitif bir sayıdır.")
+print("Bu her zaman yazdırılır.")
+
+
+sayı = -1
+if sayı > 0 :
+    print(sayı, "pozitif bir sayıdır.")
+print("Bu da her zaman yazdırılır.")
